278b249c4c95a7a116371b2b576679fd016c3523
Guruswami <gsangayyahiremath@cainc.com>
2019-07-03 13:13:51 +0530

    minor changes

--- src/RoleObjectFactory.js
+++ src/RoleObjectFactory.js
@@ -118,7 +118,6 @@ function createAccessibilityObjectForRole(config) {
     case ROLES.SEARCH:
     case ROLES.TABLEBODY:
     case ROLES.TABLEFOOT:
-    case ROLES.APPLICATION:
     case ROLES.TABLEHEAD:
       accessibilityObject = new AccessibilityObject(displayObject, role, domIdPrefix);
       break;
