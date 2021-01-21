08cc6fb854513ee4adc884b610f7ca19501b0fa8
Matt Newcomb <mnewcomb@cainc.com>
2021-01-21 13:33:03 -0500

    checking child roles for row

--- src/RoleObjects/RowData.js
+++ src/RoleObjects/RowData.js
@@ -1,6 +1,33 @@
 import GroupData from './GroupData';
+import { ROLES } from '../Roles';
 
 export default class RowData extends GroupData {
+  addChild(displayObject) {
+    if (!displayObject.accessible
+      || (displayObject.accessible.role !== ROLES.CELL
+          && displayObject.accessible.role !== ROLES.GRIDCELL
+          && displayObject.accessible.role !== ROLES.COLUMNHEADER
+          && displayObject.accessible.role !== ROLES.ROWHEADER
+      )
+    ) {
+      throw new Error(`Children of ${this.role} must have a role of ${ROLES.CELL}, ${ROLES.GRIDCELL}, ${ROLES.COLUMNHEADER}, or ${ROLES.ROWHEADER}`);
+    }
+    super.addChild(displayObject);
+  }
+
+  addChildAt(displayObject, index) {
+    if (!displayObject.accessible
+      || (displayObject.accessible.role !== ROLES.CELL
+          && displayObject.accessible.role !== ROLES.GRIDCELL
+          && displayObject.accessible.role !== ROLES.COLUMNHEADER
+          && displayObject.accessible.role !== ROLES.ROWHEADER
+      )
+    ) {
+      throw new Error(`Children of ${this.role} must have a role of ${ROLES.CELL}, ${ROLES.GRIDCELL}, ${ROLES.COLUMNHEADER}, or ${ROLES.ROWHEADER}`);
+    }
+    super.addChildAt(displayObject, index);
+  }
+
   /**
    * Sets an element's column index or position with respect to the total number
    * of columns within a table, grid, or treegrid.
