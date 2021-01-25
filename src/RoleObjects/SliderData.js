2c88792541f290e9a83c25feba5712a5623b99a9
mnewcomb314 <mnewcomb@cainc.com>
2021-01-25 13:01:36 -0500

    Allow for setting slider page up/down increments (#37)
    * prepping new version number for once this merges
    
    * making onChange a private member since it's an event listener that has no reason to be public
    
    * fixing comment spacing
    
    * adding missing function comment
    
    * allowing the page up/down increment to be specified

--- src/RoleObjects/SliderData.js
+++ src/RoleObjects/SliderData.js
@@ -1,115 +1,190 @@
+import KeyCodes from 'keycodes-enum';
 import _ from 'lodash';
 import InputTagData from './InputTagData';
 
 /**
  * Class for the slider role
  */
-
 export default class SliderData extends InputTagData {
   constructor(displayObject, role, domIdPrefix) {
     super(displayObject, role, domIdPrefix);
 
-    _.bindAll(this, 'onChange');
-    this._reactProps.onChange = this.onChange;
+    _.bindAll(this, '_onChange', '_onKeyDown');
+    this._reactProps.onChange = this._onChange;
+    this._reactProps.onKeyDown = this._onKeyDown;
     this._reactProps.type = 'range';
   }
 
-  onChange(evt) {
-    this._reactProps.value = evt.target.value;
-    const event = new createjs.Event('valueChanged', false, false);
-    event.newValue = this._reactProps.value;
-    this._displayObject.dispatchEvent(event);
+  /**
+   * @inheritdoc
+   */
+  set enableKeyEvents(enable) {
+    super.enableKeyEvents = enable;
+    // keeping the keydown listener always bound for simplicity of dealing with
+    // when pageStep is set.
+    this._reactProps.onKeyDown = this._onKeyDown;
   }
 
+  /**
+   * @inheritdoc
+   */
+  get enableKeyEvents() {
+    return super.enableKeyEvents;
+  }
 
   /**
-  * Sets the step of the slider
-  * @access public
-  * @param {Number} val - step of the slider
-  */
+   * Sets the step of the slider
+   * @access public
+   * @param {Number} val - step of the slider
+   */
   set step(val) {
     this._reactProps.step = val;
   }
 
   /**
-  * Retrieves the step of the slider
-  * @access public
-  * @returns {Number} step of the slider
-  */
+   * Retrieves the step of the slider
+   * @access public
+   * @returns {Number} step of the slider
+   */
   get step() {
     return this._reactProps.step;
   }
 
+  /**
+   * Sets the amount to adjust the slider by for page up/down of the slider.
+   * When this is field is unset, the browser default behavior will be used for
+   * those keystrokes.
+   * @access public
+   * @param {Number} val - amount to increment or decrement the slider value by
+   * when page up or page down are pressed.  undefined to unset the field.
+   */
+  set pageStep(val) {
+    this._pageStep = val;
+  }
 
   /**
-    * Sets the orientation of slider
-    * @access public
-    * @param {String} str - "horizontal" for a horizontal slider, "vertical" for a vertical slider
-    */
+   * Retrieves the amount to adjust the slider by for page up/down keystrokes
+   * @access public
+   * @returns {Number} amount to increment or decrement the slider value by when
+   * page up or down is pressed.  undefined when browser default behavior is used
+   * for those keystrokes.
+   */
+  get pageStep() {
+    return this._pageStep;
+  }
+
+  /**
+   * Sets the orientation of slider
+   * @access public
+   * @param {String} str - "horizontal" for a horizontal slider, "vertical" for a vertical slider
+   */
   set orientation(str) {
     this._reactProps['aria-orientation'] = str;
   }
 
   /**
-    * Retrieves the orientation of slider
-    * @access public
-    * @returns  {String} str "horizontal" for a horizontal slider, "vertical" for a vertical slider
-    */
+   * Retrieves the orientation of slider
+   * @access public
+   * @returns  {String} str "horizontal" for a horizontal slider, "vertical" for a vertical slider
+   */
   get orientation() {
     return this._reactProps['aria-orientation'];
   }
 
   /**
-  * Set slider value
-  * @access public
-  * @param {Number} val - slider current value
-  */
+   * Set slider value
+   * @access public
+   * @param {Number} val - slider current value
+   */
   set value(val) {
     this._reactProps.value = val;
   }
 
   /**
-  * get slider value
-  * @access public
-  * @returns {Number} -slider current value
-  */
+   * get slider value
+   * @access public
+   * @returns {Number} -slider current value
+   */
   get value() {
     return this._reactProps.value;
   }
 
   /**
-  * Set slider min value
-  * @access public
-  * @param {Number} val - slider min value
-  */
+   * Set slider min value
+   * @access public
+   * @param {Number} val - slider min value
+   */
   set min(val) {
     this._reactProps.min = val;
   }
 
   /**
-  * get slider min value
-  * @access public
-  * @returns {Number} -slider min value
-  */
+   * get slider min value
+   * @access public
+   * @returns {Number} -slider min value
+   */
   get min() {
     return this._reactProps.min;
   }
 
   /**
-  * Set slider max value
-  * @access public
-  * @param {Number} val - slider max value
-  */
+   * Set slider max value
+   * @access public
+   * @param {Number} val - slider max value
+   */
   set max(val) {
     this._reactProps.max = val;
   }
 
   /**
-  * get slider max value
-  * @access public
-  * @returns {Number} - slider max value
-  */
+   * get slider max value
+   * @access public
+   * @returns {Number} - slider max value
+   */
   get max() {
     return this._reactProps.max;
+  }
+
+  /**
+   * Event listener for React change events
+   * @access private
+   * @param {SyntheticEvent} evt - React event
+   */
+  _onChange(evt) {
+    this.value = evt.target.value;
+    const event = new createjs.Event('valueChanged', false, false);
+    event.newValue = this.value;
+    this._displayObject.dispatchEvent(event);
+  }
+
+  /**
+   * @inheritdoc
+   */
+  _onKeyDown(evt) {
+    if (this.enableKeyEvents) {
+      super._onKeyDown(evt);
+      if (evt.defaultPrevented) {
+        return;
+      }
+    }
+
+    if (!_.isUndefined(this.pageStep) && (evt.keyCode === KeyCodes.pageup || evt.keyCode === KeyCodes.pagedown)) { // eslint-disable-line max-len
+      const multiplier = evt.keyCode === KeyCodes.pageup ? 1 : -1;
+      const delta = multiplier * this.pageStep;
+      this.value = _.toNumber(this.value) + delta;
+      if (!_.isUndefined(this.min)) {
+        this.value = Math.max(this.value, this.min);
+      }
+      if (!_.isUndefined(this.max)) {
+        this.value = Math.min(this.value, this.max);
+      }
+
+      const event = new createjs.Event('valueChanged', false, false);
+      event.newValue = this.value;
+      this._displayObject.dispatchEvent(event);
+
+      evt.stopPropagation();
+      evt.preventDefault();
+    }
   }
 }
