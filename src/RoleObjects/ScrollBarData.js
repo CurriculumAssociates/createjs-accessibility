57dd6eb42fd6c9cde371abc18d79dd38e17c9bc5
Matt Newcomb <mnewcomb@cainc.com>
2021-02-11 15:15:19 -0500

    bunch of fixes to scrollbar

--- src/RoleObjects/ScrollBarData.js
+++ src/RoleObjects/ScrollBarData.js
@@ -5,56 +5,88 @@ import RangeData from './RangeData';
 export default class ScrollBarData extends RangeData {
   constructor(displayObject, role, domIdPrefix) {
     super(displayObject, role, domIdPrefix);
-    _.bindAll(this, 'onKeyDown');
-    this._reactProps.onKeyDown = this.onKeyDown;
-    this.incrementor = 20;
-    this._scrollAmt = 0;
+    _.bindAll(this, '_onKeyDown');
+    this._reactProps.onKeyDown = this._onKeyDown;
+
+    /**
+     * Amount to increment or decrement the value by when the arrow keys are pressed
+     * @access public
+     */
+    this.increment = 20;
+
+    // setup defaults per WAI-ARIA
+    this.min = 0;
+    this.max = 100;
+    this.value = (this.max + this.min) / 2;
   }
 
-  set controls(value) {
-    this._reactProps['aria-controls'] = value;
+  /**
+   * @inheritdoc
+   */
+  set enableKeyEvents(enable) {
+    super.enableKeyEvents = enable;
+    this._reactProps.onKeyDown = this._onKeyDown;
   }
 
-  get controls() {
-    return this._reactProps['aria-controls'];
+  /**
+   * @inheritdoc
+   */
+  get enableKeyEvents() {
+    return super.enableKeyEvents;
   }
 
-  set orientation(value) {
-    this._reactProps['aria-orientation'] = value;
+  /**
+   * Sets the orientation
+   * @access public
+   * @param {String} str - "horizontal" for a horizontal slider, "vertical" for a vertical scrollbar
+   */
+  set orientation(str) {
+    this._reactProps['aria-orientation'] = str;
   }
 
+  /**
+   * Retrieves the orientation
+   * @access public
+   * @returns  {String} str "horizontal" for a horizontal slider, "vertical" for a vertical scrollbar
+   */
   get orientation() {
     return this._reactProps['aria-orientation'];
   }
 
-  set scrollAmount(value) {
-    this._scrollAmt = value;
-  }
+  /**
+   * @inheritdoc
+   */
+  _onKeyDown(e) {
+    if (this.enableKeyEvents) {
+      super._onKeyDown(evt);
+      if (evt.defaultPrevented) {
+        return;
+      }
+    }
 
-  onKeyDown(e) {
     if (this.orientation !== 'horizontal') {
       if (e.keyCode === KeyCodes.up || e.keyCode === KeyCodes.down) {
         e.stopPropagation();
         e.preventDefault();
         if (e.keyCode === KeyCodes.up) {
-          this._scrollAmt -= this.incrementor;
+          this.value = Math.max(this.value - this.increment, this.min);
         } else {
-          this._scrollAmt += this.incrementor;
+          this.value = Math.min(this.value + this.increment, this.max);
         }
         const event = new createjs.Event('scroll', false, e.cancelable);
-        event.scrollTop = this._scrollAmt;
+        event.scrollTop = this.value;
         this._displayObject.dispatchEvent(event);
       }
     } else if (e.keyCode === KeyCodes.left || e.keyCode === KeyCodes.right) {
       e.stopPropagation();
       e.preventDefault();
       if (e.keyCode === KeyCodes.left) {
-        this._scrollAmt -= this.incrementor;
+        this.value = Math.max(this.value - this.increment, this.min);
       } else {
-        this._scrollAmt += this.incrementor;
+        this.value = Math.min(this.value + this.increment, this.max);
       }
       const event = new createjs.Event('scroll', false, e.cancelable);
-      event.scrollLeft = this._scrollAmt;
+      event.scrollLeft = this.value;
       this._displayObject.dispatchEvent(event);
     }
   }
