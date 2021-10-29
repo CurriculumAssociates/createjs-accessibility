d3c3fe35ccb4b1abfed80aa33105d2b495d4819c
Danny Rosenberg <drosenberg@cainc.com>
2021-10-29 16:48:19 -0400

    - cleaned up a bit

--- src/Roles.test.js
+++ src/Roles.test.js
@@ -1,7 +1,25 @@
 import { doesRoleUseSemanticallyInteractiveTag } from './Roles';
 
-describe('doesRoleUseSemanticallyInteractiveTag', () => {
-  it('returns true', () => {
-    expect(doesRoleUseSemanticallyInteractiveTag('article')).toEqual(false);
+describe('Roles', () => {
+  describe('doesRoleUseSemanticallyInteractiveTag', () => {
+    it('returns true for interactive tags', () => {
+      expect(doesRoleUseSemanticallyInteractiveTag('link')).toEqual(true);
+      expect(doesRoleUseSemanticallyInteractiveTag('multiselectlistbox')).toEqual(true);
+      expect(doesRoleUseSemanticallyInteractiveTag('checkbox')).toEqual(true);
+      expect(doesRoleUseSemanticallyInteractiveTag('radio')).toEqual(true);
+      expect(doesRoleUseSemanticallyInteractiveTag('searchbox')).toEqual(true);
+      expect(doesRoleUseSemanticallyInteractiveTag('slider')).toEqual(true);
+      expect(doesRoleUseSemanticallyInteractiveTag('spinbutton')).toEqual(true);
+      expect(doesRoleUseSemanticallyInteractiveTag('multilinetextbox')).toEqual(true);
+      expect(doesRoleUseSemanticallyInteractiveTag('button')).toEqual(true);
+      expect(doesRoleUseSemanticallyInteractiveTag('switch')).toEqual(true);
+    });
+
+    it('returns false for non-interactive tags', () => {
+      expect(doesRoleUseSemanticallyInteractiveTag('article')).toEqual(false);
+      expect(doesRoleUseSemanticallyInteractiveTag('img')).toEqual(false);
+      expect(doesRoleUseSemanticallyInteractiveTag('span')).toEqual(false);
+      expect(doesRoleUseSemanticallyInteractiveTag('table')).toEqual(false);
+    });
   });
 });
