94a0b038637ca65abfa5c41215f31210882a0247
Danny Rosenberg <drosenberg@cainc.com>
2021-10-29 15:03:29 -0400

    - removed unused code

--- jest.config.js
+++ jest.config.js
@@ -11,12 +11,6 @@ module.exports = {
   // The test environment that will be used for testing
   testEnvironment: 'jsdom',
 
-  // A map from regular expressions to paths to transformers
-  // transform: {
-  //   '^.+\\.jsx$': 'babel-jest',
-  //   '^.+\\.js$': 'babel-jest'
-  // },
-
   // The paths to modules that run some code to configure or set up the testing environment before each test
   setupFiles: ['jest-canvas-mock'],
 };
