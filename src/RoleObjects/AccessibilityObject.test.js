3585adc478a8c0e9aacf44be4be9eab58d6a204a
Tushar Rane <rtushar@cainc.com>
2022-03-04 14:07:10 +0530

    initial Commit

---
+++ src/RoleObjects/AccessibilityObject.test.js
@@ -0,0 +1,35 @@
+import * as createjs from 'createjs-module';
+import ReactTestUtils from 'react-dom/test-utils';
+import AccessibilityModule from '../index';
+
+describe('Accessibility Objects', () => {
+  it('Accessibility Object should provide event and keycode with the event', () => {
+    const keyboardClickListener = jest.fn();
+    const canvas = document.createElement('canvas');
+    const parentEl = document.createElement('div');
+    const stage = new createjs.Stage(canvas);
+    const container = new createjs.Container();
+    window.createjs = createjs;
+    AccessibilityModule.register({
+      displayObject: container,
+      role: AccessibilityModule.ROLES.MAIN,
+      accessibleOptions: { enableKeyEvents: true },
+    });
+    AccessibilityModule.setupStage(stage, parentEl);
+    stage.accessibilityTranslator.root = container;
+    stage.addChild(container);
+    container.on('keydown', keyboardClickListener);
+    stage.update();
+    stage.accessibilityTranslator.update();
+
+    [['Enter', 13], ['a', 65], ['t', 84], ['Tab', 9]].forEach(([key, keyCode], i) => {
+      ReactTestUtils.Simulate.keyDown(parentEl.querySelector('main'), { keyCode, key });
+      const keyboardListenerArgument = keyboardClickListener.mock.calls[i][0];
+      const receivedKey = keyboardListenerArgument.key;
+      const receivedkeyCode = keyboardListenerArgument.keyCode;
+
+      expect(receivedKey).toBe(key);
+      expect(receivedkeyCode).toBe(keyCode);
+    });
+  });
+});
