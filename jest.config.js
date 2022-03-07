762cd777b524fb376ec11f1ed2ddb6d48f76ce45
Danny Rosenberg <drosenberg@cainc.com>
2022-03-07 17:07:29 -0500

    -  test thresholds

--- jest.config.js
+++ jest.config.js
@@ -8,9 +8,20 @@ module.exports = {
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
+  // This will be used to configure minimum threshold enforcement for coverage results.
+  coverageThreshold: {
+    global: {
+      branches: 10,
+      functions: 5,
+      lines: 8,
+      statements: 8,
+    },
+  },
+
+  // The paths to modules that run some code to configure or set up the testing environment before
+  // each test
+  setupFiles: ['jest-canvas-mock'],
+
   // The test environment that will be used for testing
   testEnvironment: 'jsdom',
-
-  // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ['jest-canvas-mock'],
 };
