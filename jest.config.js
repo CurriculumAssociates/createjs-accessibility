83284ee4b83d2cdb982eb3e4ff5c9b5af0755102
drosenberg123 <89026334+drosenberg123@users.noreply.github.com>
2022-03-11 11:29:33 -0500

    Wire-up Unit Test coverage reports in GitHub (#83)
    * - testing `jest-coverage-report-action`
    
    * - added `pull_request` to `on:`
    - changed `push.yml` to `checks.yml` to more accurately reflect the use case
    
    * - changed 'name'
    
    * -  test thresholds
    
    * - broke something
    
    * - 2nd try
    
    * - tried lowering coverage #s
    
    * - bumped version

--- jest.config.js
+++ jest.config.js
@@ -8,9 +8,20 @@ module.exports = {
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
+  // This will be used to configure minimum threshold enforcement for coverage results.
+  coverageThreshold: {
+    global: {
+      branches: 10,
+      functions: 4,
+      lines: 7,
+      statements: 7,
+    },
+  },
+
+  // The paths to modules that run some code to configure or set up the testing environment before
+  // each test
+  setupFiles: ['jest-canvas-mock'],
+
   // The test environment that will be used for testing
   testEnvironment: 'jsdom',
-
-  // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ['jest-canvas-mock'],
 };
