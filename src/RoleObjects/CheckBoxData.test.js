2924449a3c07fcb2e27a21afee218df1f243299f
Danny Rosenberg <drosenberg@cainc.com>
2022-04-12 20:33:52 -0400

    - PR requests/enhancements - added test helper file `__jestSharedSetup.js` from @mmoss

--- src/RoleObjects/CheckBoxData.test.js
+++ src/RoleObjects/CheckBoxData.test.js
@@ -1,35 +1,14 @@
 import * as createjs from 'createjs-module';
-import 'core-js/stable';
-import ReactTestUtils from 'react-dom/test-utils';
-import KeyCodes from 'keycodes-enum';
 import AccessibilityModule from '../index';
 import { parentEl, stage, container } from '../__jestSharedSetup';
 
 describe('CheckBoxData', () => {
   describe('setupStage', () => {
-    window.createjs = createjs;
-    let canvasEl;
-    let parentEl;
-    let stage;
-    let container;
     let cjsCheckbox;
     let checkboxEl;
 
     beforeEach(() => {
-      // const keyboardClickListener = jest.fn();
-      canvasEl = document.createElement('canvas');
-      parentEl = document.createElement('div');
-      stage = new createjs.Stage(canvasEl);
-      container = new createjs.Container();
       cjsCheckbox = new createjs.Shape();
-
-      AccessibilityModule.register({
-        displayObject: container,
-        role: AccessibilityModule.ROLES.MAIN,
-      });
-      AccessibilityModule.setupStage(stage, parentEl);
-      stage.accessibilityTranslator.root = container;
-      stage.addChild(container);
 
       AccessibilityModule.register({
         displayObject: cjsCheckbox,
@@ -38,14 +17,13 @@ describe('CheckBoxData', () => {
       });
 
       cjsCheckbox.accessible.enableKeyEvents = true;
-      // container.on('keydown', keyboardClickListener);
-      // stage.update();
       stage.accessibilityTranslator.update();
+
+      checkboxEl = parentEl.querySelector('input');
     });
 
     it('creates input[type=checkbox] element', () => {
-      checkboxEl = parentEl.querySelector('input[type=checkbox]');
-      expect(checkboxEl).not.toBeNull();
+      expect(checkboxEl.type).toEqual('checkbox');
     });
 
     it('disables keyboard events', () => {
@@ -54,28 +32,11 @@ describe('CheckBoxData', () => {
     });
 
     it('can read and set "checked" property', () => {
+      // False by default
+      expect(cjsCheckbox.accessible.checked).toEqual(false);
+
       cjsCheckbox.accessible.checked = true;
       expect(cjsCheckbox.accessible.checked).toEqual(true);
-
-      // console.log('before keyDown', checkboxEl.checked);
-      ReactTestUtils.Simulate.keyDown(checkboxEl, { key: 'Enter', keyCode: KeyCodes.enter });
-      // console.log('after', checkboxEl.checked);
-      // expect(cjsCheckbox.accessible.checked).toEqual(false);
-
-      // console.log('before change', checkboxEl.checked);
-      ReactTestUtils.Simulate.change(checkboxEl, { target: { checked: true } });
-      // console.log('after', checkboxEl.checked);
-      // expect(cjsCheckbox.accessible.checked).toEqual(true);
-      // expect(keyboardClickListener).toHaveBeenCalled();
-
-      stage.update();
-      stage.accessibilityTranslator.update(() => {
-        // console.log('update callback()', checkboxEl.outerHTML);
-      });
-
-      return new Promise(resolve => setImmediate(resolve)).then(() => {
-        // console.log('please?', checkboxEl.checked);
-      });
     });
   });
 });
