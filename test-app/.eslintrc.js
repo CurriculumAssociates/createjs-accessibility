116c4b0f43de5875bc5072afa09b421797b0a2d5
Danny Rosenberg <drosenberg@cainc.com>
2022-08-23 10:53:08 -0400

    fix for linting collisions between root and /test-app

--- test-app/.eslintrc.js
+++ test-app/.eslintrc.js
@@ -1,7 +1,15 @@
 module.exports = {
   rules: {
+    'array-bracket-newline': 'warn',
+    'class-methods-use-this': 'warn',
     'import/no-unresolved': 'off',
+    'no-underscore-dangle': 'off',
+    'no-unused-expressions': 'warn',
+    'no-undef': 'warn',
+    'no-param-reassign': 'warn',
+    'no-plusplus': 'warn'
   },
+  extends: ['airbnb', 'prettier', 'prettier/prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
