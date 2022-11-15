5122829dcd36d72886303aa22c3f5d62bc653ab4
Tushar Rane <rtushar@cainc.com>
2022-11-15 15:32:51 +0530

    updated .tsx files

---
+++ src/index.ts
@@ -0,0 +1,154 @@
+import _ from 'lodash';
+import AccessibilityTranslator from './AccessibilityTranslator';
+import { createAccessibilityObjectForRole } from './RoleObjectFactory';
+import { ROLES } from './Roles';
+import { createElement } from './utils/utils';
+
+interface CSSPropType {
+  [key: string]: {
+    [key: string]: string | number | boolean;
+  };
+}
+
+/**
+ * Calculates style metrics for DOM representation of canvas stage.
+ * @param {createjs.Stage} stage - createjs stage that has been registered for accessibility
+ * @returns {Object} style object for positioning associated DOM elements
+ */
+function calcDomStylesFromStage(stage): CSSPropType {
+  // true to return style object for putting the DOM element adjacent to the
+  // canvas (useful for debugging),
+  // false to return style object to put the DOM beneath the canvas
+  const debugPos = false;
+
+  const { canvas } = stage;
+
+  const computedStyle = getComputedStyle(canvas);
+  const width = parseInt(computedStyle.width, 10);
+  const height = parseInt(computedStyle.height, 10);
+
+  const attrWidth = parseInt(canvas.getAttribute('width'), 10) || width;
+  const attrHeight = parseInt(canvas.getAttribute('height'), 10) || height;
+
+  const scaleX = width / attrWidth;
+  const scaleY = height / attrHeight;
+
+  return {
+    transformStyle: {
+      overflow: 'hidden',
+      position: 'absolute',
+      left: debugPos ? canvas.offsetLeft + attrWidth * scaleX : 'auto',
+      top: `${canvas.offsetTop}px`,
+      zIndex: debugPos ? 'auto' : -1,
+      height: `${attrHeight}px`,
+      width: `${attrWidth}px`,
+      marginLeft: computedStyle['margin-left'],
+      transform: computedStyle.transform,
+      transformOrigin: computedStyle.transformOrigin,
+    },
+    moduleStyle: {
+      width: '100%',
+      height: '100%',
+      border: computedStyle.border,
+      boxSizing: computedStyle['box-sizing'],
+      padding: computedStyle.padding,
+      transform: `scaleX(${scaleX}) scaleY(${scaleY})`,
+      transformOrigin: 'top left',
+    },
+  };
+}
+
+/**
+ * Setup accessibility support for a Stage. The AccessibilityTranslator instance will be
+ * attached to the provided stage by adding an "accessibilityTranslator" member to the stage.
+ * @param {!createjs.Stage} stage - CreateJS Stage to attach the translator
+ * @param {!(HTMLElement|string)} parentElement - HTML Element or its id to which the DOM
+ * translation will be added
+ * @param {Function} onReady - An optional function to call when the module is mounted into the
+ * parentElement
+ */
+function setupStage(stage, parentElement, onReady = () => {}) {
+  const component = new AccessibilityTranslator();
+  const styles: CSSPropType = calcDomStylesFromStage(stage);
+  const moduleNode = createElement('div', { style: styles.transformStyle }, [
+    {
+      tagName: 'div',
+      props: { style: styles.moduleStyle },
+      childElements: [{ tagName: 'div', props: { id: 'root' } }],
+    },
+  ]);
+
+  if (_.isString(parentElement)) {
+    parentElement = document.getElementById(parentElement);
+  }
+
+  parentElement.replaceChildren(moduleNode);
+  stage.accessibilityTranslator = component;
+  stage.accessibilityRootElement = parentElement;
+  component.createDomTree();
+  onReady();
+}
+
+/**
+ * Cleanup and unmount node that tracks accessibility support for a Stage.
+ * @param {!createjs.Stage} stage - CreateJS Stage to cleanup
+ */
+function releaseStage(stage) {
+  const accessibilityRoot = stage.accessibilityRootElement;
+  accessibilityRoot.replaceChildren();
+}
+
+/**
+ * Handles the stage being resized so that the accessibility DOM translation stays under it with
+ * the same dimensions
+ * @param {!createjs.Stage} stage - stage that has been registered for accessibility support that
+ * has been resized
+ */
+function resize(stage) {
+  if (stage.accessibilityTranslator.rootElem) {
+    const camWrapperElem =
+      stage.accessibilityTranslator.rootElem.parentElement.parentElement;
+    const { transformStyle }: CSSPropType = calcDomStylesFromStage(stage);
+    _.keys(transformStyle).forEach((style) => {
+      camWrapperElem.style[style] = transformStyle[style];
+    });
+  } else {
+    // handle this function being called before React sets the ref for rootElem
+    _.defer(() => {
+      resize(stage);
+    });
+  }
+}
+
+/**
+ * Takes either an array or a single object and creates the accessibility object for each with the
+ * config that's passed
+ * @param {Array | Object} configObjects Takes either an array or a single object
+ * @returns {Object} returns accessibility object
+ */
+function register(configObjects) {
+  let objects = configObjects;
+
+  if (!Array.isArray(configObjects)) {
+    objects = [configObjects];
+  }
+
+  const accessiblityObjects = objects.map((objectConfig) =>
+    createAccessibilityObjectForRole(objectConfig)
+  );
+
+  return accessiblityObjects.length > 1
+    ? accessiblityObjects
+    : accessiblityObjects[0];
+}
+
+const AccessibilityModule = {
+  createAccessibilityObjectForRole,
+  register,
+  resize,
+  ROLES,
+  setupStage,
+  releaseStage,
+};
+
+export default AccessibilityModule;
---
+++ src/utils/utils.js
@@ -0,0 +1,131 @@
+/* eslint-disable */
+import { depth } from 'treeverse';
+import _ from 'lodash';
+
+const updateAttributesFromProps = (element, props) => {
+  Object.entries(props || {}).forEach(([name, value]) => {
+    if (name.startsWith('on')) {
+      element.addEventListener(name.toLowerCase().substring(2), value);
+    } else if (name === 'style' && _.isObject(value)) {
+      const valueString = Object.entries(value)
+        .map(([k, v]) => {
+          k = k.replace(/[A-Z]/g, (match) => `-${match.toLowerCase()}`);
+          return `${k}: ${v}`;
+        })
+        .join('; ');
+      element.setAttribute('style', valueString);
+    } else {
+      if (name === 'acceptCharset') {
+        name = name.replace(/[A-Z]/g, (match) => `-${match.toLowerCase()}`);
+      }
+      element.setAttribute(name, value !== undefined ? value.toString() : '');
+    }
+  });
+};
+
+export const findDomDataObjFromDomId = (domData, domId) => {
+  let returnObj = null;
+  depth({
+    tree: domData,
+    visit(node) {
+      if (node.props && node.props.id === domId) {
+        returnObj = node;
+        return node;
+      }
+    },
+    getChildren(node) {
+      return node.childElements;
+    },
+  });
+  return returnObj;
+};
+
+export const createElement = (tagName, props, children = []) => {
+  const element = document.createElement(tagName);
+
+  updateAttributesFromProps(element, props);
+
+  if (children && children.length > 0) {
+    children.forEach((child, index) => {
+      insertChildAtIndex(element, child, index);
+    });
+  }
+
+  return element;
+};
+
+const insertChildAtIndex = (parent, child, index) => {
+  const childElem = child.tagName
+    ? createElement(child.tagName, child.props, child.childElements)
+    : document.createTextNode(child);
+  if (
+    index < parent.childNodes.length &&
+    parent.childNodes[index].isEqualNode(childElem)
+  ) {
+    return;
+  }
+  if (index === undefined || index >= parent.childNodes.length) {
+    parent.appendChild(childElem);
+  } else {
+    parent.insertBefore(childElem, parent.childNodes[index]);
+  }
+};
+
+export const updateElement = (displayObj, domDataObj, domId) => {
+  const { tagName, props, childElements } = findDomDataObjFromDomId(
+    domDataObj,
+    domId
+  );
+  const element = document.querySelector(`#${domId}`);
+  if (element) {
+    updateAttributesFromProps(element, props);
+    const elementsToRemove = [];
+    element.childNodes.forEach((childNode) => {
+      if (
+        !childElements.some((obj) => {
+          return _.isString(obj)
+            ? obj === childNode
+            : obj.props.id === childNode.getAttribute('id');
+        })
+      ) {
+        elementsToRemove.push(childNode);
+      }
+    });
+    elementsToRemove.forEach((el) => element.removeChild(el));
+
+    childElements.forEach((childElData, index) => {
+      insertChildAtIndex(element, childElData, index);
+    });
+  } else {
+    addMissingElem(displayObj, { tagName, props });
+  }
+};
+
+export const getUpdatedDisplayObjects = (displayObject) => {
+  const updatedElements = [];
+  depth({
+    tree: displayObject,
+    visit(node) {
+      if (node.accessible._markedForUpdate) {
+        updatedElements.push(node);
+      }
+    },
+    getChildren(node) {
+      return node.accessible.children;
+    },
+  });
+  return updatedElements;
+};
+
+export const addMissingElem = (displayObj, { tagName, props }) => {
+  let parentElem;
+  if (displayObj.accessible.parent) {
+    const parentId = displayObj.accessible.parent._domId;
+    parentElem = document.querySelector(`#${parentId}`);
+  } else {
+    parentElem = document.querySelector('#root');
+  }
+  const domElem = createElement(tagName, props);
+  parentElem.insertAdjacentElement('afterbegin', domElem);
+  return domElem;
+};
