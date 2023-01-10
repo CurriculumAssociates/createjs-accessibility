f4c0435444b168e5ad11b9958f5a377c027ca392
Danny Rosenberg <drosenberg@cainc.com>
2023-01-10 14:19:32 -0500

    - added missing package (`@babel/preset-env`) and associated webpack config

--- test-app/webpack.config.js
+++ test-app/webpack.config.js
@@ -27,7 +27,27 @@ const config = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: [{ loader: 'babel-loader' }],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env',
+                  {
+                    modules: false,
+                    targets: {
+                      chrome: '88',
+                      ie: '11',
+                      edge: '88',
+                      firefox: '79',
+                      safari: '13.1.3',
+                    },
+                  }
+                ],
+              ],
+            },
+          },
+        ],
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
