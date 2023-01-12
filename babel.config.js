aaf4a802dbd97fa9f0dff60a82f13d2b886497f5
Danny Rosenberg <drosenberg@cainc.com>
2023-01-12 16:42:43 -0500

    - fix roots so babel doesn't try to apply ./babel.config.js to /test-app

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,9 @@
 module.exports = function babelConfig(api) {
   return {
+    babelrcRoots: [
+      '.',
+      './test-app',
+    ],
     presets: [
       ['@babel/preset-env', { modules: api.env(['test']) ? 'auto' : false }],
       '@babel/preset-typescript',
